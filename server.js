701d57fc0ec7b66fcb495c138396cbb95e58cb42
javlonumrzokov <jackumrzokov@gmail.com>
2024-08-13 23:05:15 +0900

    feat: EJS orqali ananaviy frontend qurdik

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4:  Routing code
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
+});
 app.get("/", function (req, res) {
-  res.end(`<h1>Home page</h1>`);
-});
-app.get("/hello", function (req, res) {
-  res.end(`<h1 style="background:red">HELLO WORLD from Javlon</h1>`);
-});
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
+  res.render("harid");
 });
 
 const server = http.createServer(app);
