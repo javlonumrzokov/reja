518fe2056179cb40118ee38e0b6d905917605aa5
javlonumrzokov <jackumrzokov@gmail.com>
2024-08-16 04:53:08 +0900

    feat: develop author portfolio page

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set("view engine", "ejs");
 
 // 4:  Routing code
 app.post("/create-item", (req, res) => {
-  // TODO: code with db here
+  // TODO: code with db hereb
 });
 
 app.get("/author", (req, res) => {
