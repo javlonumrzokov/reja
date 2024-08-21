7ace46e9554197bbbd6db444132b0cdc0d784e22
javlonumrzokov <jackumrzokov@gmail.com>
2024-08-21 13:24:04 +0900

    fix: cleared the extra lines

--- train.js
+++ train.js
@@ -7,7 +7,7 @@
 //   "yoshlarga invest qiling", //50-60
 //   "endi dam oling, foydasi yoq", //60~
 // ];
-// CALLBACK functions with setTimeout
+// CALLBACK functions
 // function maslahatBering(a, callback) {
 //   if (typeof a !== "number") callback("please, insert a number", null);
 //   else if (a <= 20) callback(null, list[0]);
@@ -16,9 +16,12 @@
 //   else if (a > 40 && a <= 50) callback(null, list[3]);
 //   else if (a > 50 && a <= 60) callback(null, list[4]);
 //   else {
-//     setTimeout(function () {
+//// setTimeout(function () {
+// //   callback(null, list[5]);
+// // }, 5000);
+//     setInterval(function () {
 //       callback(null, list[5]);
-//     }, 5000);
+//     }, 1000);
 //   }
 // }
 
@@ -27,6 +30,7 @@
 //   if (err) console.log("ERROR:", err);
 //   console.log("javob:", data);
 // });
+// console.log("passed here 1");
 
 //Asynch function
 // async function maslahatBering(a) {
@@ -73,28 +77,6 @@
 //   console.log(javob);
 // }
 // run();
-
-//CALLBACK  setInterval bilan
-// function maslahatBering(a, callback) {
-//   if (typeof a !== "number") callback("please, insert a number", null);
-//   else if (a <= 20) callback(null, list[0]);
-//   else if (a > 20 && a <= 30) callback(null, list[1]);
-//   else if (a > 30 && a <= 40) callback(null, list[2]);
-//   else if (a > 40 && a <= 50) callback(null, list[3]);
-//   else if (a > 50 && a <= 60) callback(null, list[4]);
-//   else {
-//     setInterval(function () {
-//       callback(null, list[5]);
-//     }, 1000);
-//   }
-// }
-// console.log("passed here 0");
-// maslahatBering(67, (err, data) => {
-//   if (err) console.log("ERROR:", err);
-//   console.log("javob:", data);
-// });
-
-// console.log("passed here 1");
 
 // A-TASK:
 
