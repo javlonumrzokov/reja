4d4ddf885270a89ea6b89bcc891e3e87a45da531
javlonumrzokov <jackumrzokov@gmail.com>
2024-08-25 00:56:09 +0900

    feat: CRUD operations dan delete ni bajarish

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 
 // MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
@@ -33,6 +34,16 @@ app.post("/create-item", (req, res) => {
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
     res.json(data.ops[0]);
   });
+});
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
 });
 
 app.get("/author", (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -36,3 +36,24 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
       console.log("Iltimos, qaytadan harakat qiling");
     });
 });
+
+document.addEventListener("click", function (e) {
+  // delete operations
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq o'chirmoqchimisiz?")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log("Iltimos, qaytadan harakat qiling");
+        });
+    }
+  }
+  // edit operations
+  if (e.target.classList.contains("edit-me")) {
+    alert("siz edit tugmasini bosdingiz");
+  }
+});
