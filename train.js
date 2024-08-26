f20d40cf2c15911ac9347cbc1a8128ca0e3299b5
javlonumrzokov <jackumrzokov@gmail.com>
2024-08-27 01:45:48 +0900

    feat: Reja loyihamizning yakuniy bosqich logic i va MI D-TASK

--- train.js
+++ train.js
@@ -136,10 +136,71 @@
 // Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
 // MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
 
-const checkContent = (str1, str2) => {
-  const sorted1 = str1.split("").sort().join("");
-  const sorted2 = str2.split("").sort().join("");
-  return sorted1 === sorted2 ? true : false;
-};
-const result = checkContent("mitgroup", "gmtiprou");
-console.log("result:", result);
+// const checkContent = (str1, str2) => {
+//   const sorted1 = str1.split("").sort().join("");
+//   const sorted2 = str2.split("").sort().join("");
+//   return sorted1 === sorted2 ? true : false;
+// };
+// const result = checkContent("mitgroup", "gmtiprou");
+// console.log("result:", result);
+
+// D-TASK:
+
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda
+// classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va
+// 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+const moment = require("moment");
+
+class Shop {
+  constructor(product1, product2, product3) {
+    this.product1 = product1;
+    this.product2 = product2;
+    this.product3 = product3;
+  }
+  qoldiq() {
+    const time = moment().format("HH:mm");
+    console.log(
+      `hozir ${time} da ${this.product1}ta non, ${this.product2}ta lagmon va ${this.product3}ta cola mavjud!`
+    );
+  }
+  sotish(productName, amount) {
+    switch (productName) {
+      case "non":
+        this.product1 >= amount
+          ? (this.product1 -= amount)
+          : console.log("bizda buncha mahsulot yetarli emas");
+        break;
+      case "lagmon":
+        this.product2 >= amount
+          ? (this.product2 -= amount)
+          : console.log("bizda buncha mahsulot yetarli emas");
+        break;
+      case "cola":
+        this.product3 >= amount
+          ? (this.product3 -= amount)
+          : console.log("bizda buncha mahsulot yetarli emas");
+        break;
+      default:
+        console.log("Siz kiritgan mahsulot nomi bizda yoq");
+        break;
+    }
+  }
+  qabul(productName, amount) {
+    switch (productName) {
+      case "non":
+        this.product1 += amount;
+        break;
+      case "lagmon":
+        this.product2 += amount;
+        break;
+      case "cola":
+        this.product3 += amount;
+        break;
+      default:
+        console.log("Siz kiritgan mahsulot nomi bizda yoq");
+        break;
+    }
+  }
+}
